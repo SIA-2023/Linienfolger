9500fbcacbe562ff9a8561a14ea17722266ce8e1
Peanutt42 <p3anutt42@gmail.com>
2024-03-15 17:50:38 +0100

    Fürs erste sind ist die Fernbedienung deaktiviert (noch nicht getestet))

--- Linienfolger.ino
+++ Linienfolger.ino
@@ -1,15 +1,18 @@
 #include "Linienfolger.h"
 #include "Fernbedienung.h"
 
+// Fernbedienung wurde noch nicht getestet!
 Fernbedienung fernbedienung;
 Linienfolger linienfolger;
 
 void setup() {
 	fernbedienung.setup();
 	linienfolger.setup();
+	linienfolger.mode = Linienfolger::Mode::FolgeLinie;
 }
 
 void loop() {
+	/* Wurde noch nicht getested!
 	if (fernbedienung.has_received_new()) {
 		switch (fernbedienung.get_current_command()) {
 		case Fernbedienung::Command::Taster1:	linienfolger.mode = Linienfolger::Mode::FolgeLinie; break;
@@ -17,6 +20,7 @@ void loop() {
 		case Fernbedienung::Command::Taster3:	linienfolger.mode = Linienfolger::Mode::RechtsAusweichen; break;
 		}
 	}
+	*/
 
 	linienfolger.update();
 }
